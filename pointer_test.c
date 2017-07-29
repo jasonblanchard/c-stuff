c873b905e06250361721d5159c5b716290ab83d1
Jason Blanchard <jas.blanchard@gmail.com>
2017-07-29 17:02:04 -0400

    asdf

--- pointer_test.c
+++ pointer_test.c
@@ -4,8 +4,9 @@ int main() {
   int a = 666;
   int c = 222;
   int *p = &a;
-  // printf("%i\n", a);
+  int d = *p + c;
+  // printf("%i\n", d);
   // printf("%i\n", *p);
-  int *b = p++;
-  printf("%i\n", *b);
+  // int *b = p++;
+  // printf("%i\n", *b);
 }
